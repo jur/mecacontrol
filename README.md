38359820013a442e25946401863f244da19aea15
Jürgen Urban <JuergenUrban@gmx.de>
2020-06-13 16:39:30 +0200

    Translate names to key codes.

--- README.md
+++ README.md
@@ -6,49 +6,9 @@ python and bash are required.
 
 Example:
 Select next channel:
-./mecacontrol_send.sh 'Metz-TV' '46'
+./mecacontrol_send.sh 'Metz-TV' 'channel+'
 
 'Metz-TV' is the name configured on the TV.
-'46' is the key code for selecting next channel.
+'channel+' is the key code for selecting next channel.
 
-The key codes are:
-46 channel+
-47 channel-
-27 volume up
-28 volume down
-1-10 buttons 0-9
-39 OK
-23 up
-24 down
-25 left
-26 right
-43 play
-41 backwards
-42 forwards
-44 stop
-33 back
-40 exit
-35 sound off
-35 sound on
-36 TEXT
-37 EPG
-38 MENU
-18 blue
-19 yellow
-20 black/grey
-21 green
-22 red
-64 jump to start
-65 jump to end
-45 record
-34 pause
-255 16 HDMI 1
-255 17 HDMI 2
-255 18 HDMI 3
-255 19 HDMI 4
-255 20 Euro
-255 21 Cinch AV
-255 22 Cinch Audio
-255 0 TV modus
-51 power off
-52 power on
+Open mecacontrol_send.sh in a text editor to see possible key codes.
--- mecacontrol_send.sh
+++ mecacontrol_send.sh
@@ -10,12 +10,75 @@ cd "${BASEDIR}" || exit 1
 #
 # See mecacontrol_key.py for key and device codes.
 
+declare -A KEYCODES
+
+KEYCODES["channel+"]="46"
+KEYCODES["channel-"]="47"
+KEYCODES["volumeup"]="27"
+KEYCODES["volumedown"]="28"
+KEYCODES["button0"]="1"
+KEYCODES["button1"]="2"
+KEYCODES["button2"]="3"
+KEYCODES["button3"]="4"
+KEYCODES["button4"]="5"
+KEYCODES["button5"]="6"
+KEYCODES["button6"]="7"
+KEYCODES["button7"]="8"
+KEYCODES["button8"]="9"
+KEYCODES["button9"]="10"
+KEYCODES["ok"]="39"
+KEYCODES["up"]="23"
+KEYCODES["down"]="24"
+KEYCODES["left"]="25"
+KEYCODES["right"]="26"
+KEYCODES["play"]="43"
+KEYCODES["backward"]="41"
+KEYCODES["forward"]="42"
+KEYCODES["stop"]="44"
+KEYCODES["back"]="33"
+KEYCODES["exit"]="40"
+KEYCODES["soundoff"]="35"
+KEYCODES["text"]="36"
+KEYCODES["epg"]="37"
+KEYCODES["menu"]="38"
+KEYCODES["blue"]="18"
+KEYCODES["yellow"]="19"
+KEYCODES["black"]="20"
+KEYCODES["green"]="21"
+KEYCODES["red"]="22"
+KEYCODES["jumpstart"]="64"
+KEYCODES["jumpend"]="65"
+KEYCODES["record"]="45"
+KEYCODES["pause"]="34"
+KEYCODES["HDMI1"]="255 16"
+KEYCODES["HDMI2"]="255 17"
+KEYCODES["HDMI3"]="255 18"
+KEYCODES["HDMI4"]="255 19"
+KEYCODES["euro"]="255 20"
+KEYCODES["av"]="255 21"
+KEYCODES["audio"]="255 22"
+KEYCODES["tv"]="255 0"
+KEYCODES["poweroff"]="51"
+KEYCODES["poweron"]="52"
+
 TVNAME="${1:-Metz-TV}"
 shift
+
+declare -a PARAMS
+for KEY in "${@}"; do
+	if [ "${KEYCODES[${KEY}]}" != "" ]; then
+		for K in $(echo "${KEYCODES[${KEY}]}"); do
+			PARAMS+=("${K}")
+		done
+	else
+		PARAMS+=("${KEY}")
+	fi
+done
+
 CTRLURL="$("${SCRIPTDIR}/mecacontrol_find.sh" "${TVNAME}")"
 
 if [ "${CTRLURL}" != "" ]; then
-	"${SCRIPTDIR}/mecacontrol_key.py" "${CTRLURL}" "${@}" || exit 1
+	"${SCRIPTDIR}/mecacontrol_key.py" "${CTRLURL}" "${PARAMS[@]}" || exit 1
 else
 	echo >&2 "Error: TV not found."
 	exit 1
